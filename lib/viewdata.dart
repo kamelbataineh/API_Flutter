eaaca57e4fd8e88aaf7c2e496dcf43a390f08415
kamelbataineh <batainehkamel2@gmail.com>
2025-07-02 12:40:58 +0300

    Initial commit

---
+++ lib/viewdata.dart
@@ -0,0 +1,52 @@
+import 'package:api_flutter/Product.dart';
+import 'package:api_flutter/User.dart';
+import 'package:flutter/material.dart';
+
+
+class Viewdata extends StatefulWidget {
+  const Viewdata({super.key});
+
+  @override
+  State<Viewdata> createState() => _ViewdataState();
+}
+
+class _ViewdataState extends State<Viewdata> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+            child: ElevatedButton(
+            onPressed: ()
+                {
+                  Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Product()
+            ),
+                  );
+                 },
+                 child:Text("GET Product") ,)
+                ),
+            SizedBox(height: 10,),
+            Center(
+
+                child: ElevatedButton(
+            onPressed: ()
+                {
+                  Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => User()
+            ),
+                  );
+                 },
+                 child:Text("GET Info User") ,)
+                ),
+          ],
+        )
+    );
+    }
+  }
